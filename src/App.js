a51fb374ce4b47862c5525db5b17c25f04d1e6a2
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-02-17 02:23:13 +0800

    React casestudy#1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Monsters NFT</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
-
 export default App;
+
+// import "./App.css";
+//
+// import { useEffect, useState } from "react";
+//
+// import { CardList } from "./components/card-list/card-list.component";
+//
+// function App() {
+//   const [monsters, setMonsters] = useState([]);
+//   const [searchField, setSearchField] = [""]
+//
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) => setMonsters(users));
+//   }, []);
+//
+//   return (
+//     <div className="App">
+//       <input type="search" placeholder="search monsters" />
+//       <CardList monsters={monsters}></CardList>
+//     </div>
+//   );
+// }
+// export default App;
